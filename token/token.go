ca99f3e12c49709a6ad410f699582810307d8b17
Corey Schaf <cschaf@gmail.com>
2019-11-11 12:52:14 -0500

    adds lexer, tests and tokens for initial subset of language processor

---
+++ token/token.go
@@ -0,0 +1,50 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+var keywords = map[string]TokenType{
+	"fn":  FUNCTION,
+	"let": LET,
+}
+
+// LookupIdent checks the keywords table to see whether the given
+// identifier is in fact a keyword.  If it is, it returns the keyword's
+// TokenType constant.  If it isnt, we just get back token.IDENT,
+// which is the TokenType for all user-defined identifiers.
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// Identifiers and literals
+	IDENT = "IDENT" // add, foobar, x, y
+	INT   = "INT"   // 123456
+
+	// Operators
+	ASSIGN = "="
+	PLUS   = "+"
+
+	// Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
