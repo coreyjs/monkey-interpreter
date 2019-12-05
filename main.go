be7308f79bc4bcf4c8ec94a529e0ca9ade451acf
Corey Schaf <cschaf@gmail.com>
2019-12-05 17:23:16 -0500

    Adds REPL

--- main.go
+++ main.go
@@ -2,8 +2,20 @@ package main
 
 import (
 	"fmt"
+	"monkey/repl"
+	"os"
+	"os/user"
 )
 
 func main() {
-	fmt.Println("Parser")
+
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Monkey Language v 0.0.1;  Welcome %s\n", user.Username)
+	fmt.Printf("REPL\n")
+
+	repl.Start(os.Stdin, os.Stdout)
 }
